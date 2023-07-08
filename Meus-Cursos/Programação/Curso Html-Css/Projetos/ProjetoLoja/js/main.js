32f7c067c362ca215c8c1cfae0b084515776b5d8
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-08 18:34:50 -0300

    .

--- Meus-Cursos/Programação/Curso Html-Css/Projetos/ProjetoLoja/js/main.js
+++ Meus-Cursos/Programação/Curso Html-Css/Projetos/ProjetoLoja/js/main.js
@@ -1,12 +1,40 @@
-let secaoProdutos = document.getElementById('itens')
-// let secaoProdutos = [...secaoProdutos1]
+// Variaveis necessárias
+const main = document.getElementsByTagName('main')[0]
+const divs = document.getElementsByClassName('produtos');
+const sectionPrincipal = document.getElementById('itens');
+const conteudo = document.getElementsByClassName('conteudo');
+const imagens = document.getElementsByClassName('imgs');
+const btns1 = document.getElementsByClassName('btn1');
+const btns2 = document.getElementsByClassName('btn2');
+const burguer = document.getElementById('burguer')
+const menuflutuante = document.getElementById('menuflutuante');
 
+
+// Mneu flutuante
+
+burguer.addEventListener('click',()=>{
+    if(menuflutuante.style.display=='flex'){
+        menuflutuante.style.display='none'
+    }else{
+        menuflutuante.style.display='flex'
+    }
+})
+
+// Criação dinâmicas de divs/sections
 
 function CriarDiv(qtd){
+    let secaoProdutos = document.createElement('section')
+    secaoProdutos.setAttribute('id','itens')
+    
     let c = 1
-    while (c<=qtd){
-        let divs = document.createElement('div')
-
+    
+    while (c <= qtd){
+        
+        const criarDivs = document.createElement('div')
+        criarDivs.setAttribute('class','produtos')
+        
+        // criando seção
+        
         const criarSecao = document.createElement('section')
         criarSecao.setAttribute('class','conteudo')
 
@@ -17,51 +45,74 @@ function CriarDiv(qtd){
 
         // criando imagem
 
-        let criarImg = document.createElement('img')
-        criarImg.setAttribute('class','img')
-        criarImg.src=''
+        let criarImage = document.createElement('img')
+        criarImage.setAttribute('class','imgs')
+        criarImage.src='./img/monalisa.jpg'
+           
+        // Armazenando imagem
 
-        CriarDivimg.appendChild(criarImg)
+        CriarDivimg.appendChild(criarImage)
 
         criarSecao.appendChild(CriarDivimg)
         
-        // craindo botao
-        for(vezes=1 ; vezes<=2;vezes++){
+        // Criando botao
+        let t =0
+
+        for(vezes=1 ; vezes <= 2; ++vezes){
+
             var criarBtn = document.createElement('div')
             criarBtn.setAttribute('class','btns')
             
-            // criandoinputs para o botao
+            // Criando inputs para os botões
             
-            let criarInput = document.createElement('input')
+            var criarInput = document.createElement('input')
             criarInput.setAttribute('class','btn'+vezes)
             criarInput.setAttribute('type','button')
             criarInput.style.cursor='pointer'
             criarBtn.appendChild(criarInput)
+            if(t%2==0){
+                criarInput.value='Ver descrição'
+                t++
+
+            }else{
+                criarInput.value='Comprar'
+            }    
+            
+            // armazenando botao
+            
+            
+            // adicionando valores aos botoes     
             criarSecao.appendChild(criarBtn)
             
-            // adicionando valores aos botoes
-            var btn1 = [...document.getElementsByClassName('btn1')]
-            
-            var btn2 = [...document.getElementsByClassName('btn2')]
-            btn1.map((el)=>{
-                
-                el.value='Ver descrição'
-            })
-            btn2.map((el)=>{
-                
-                el.value='Comprar'
-            })
-            
-            
-            
-            
-        }
+        }    
         
-        divs.setAttribute('class','produtos')
-        divs.appendChild(criarSecao)
-        secaoProdutos.appendChild(divs)
+        criarDivs.appendChild(criarSecao)
+        secaoProdutos.appendChild(criarDivs)
+        main.appendChild(secaoProdutos)
         c++
-    }
-}
+    }    
+}    
 CriarDiv(9)
+
+function Criarsection(qtd){
+    let c = 1
+    while (c <= qtd){
+        let section =  document.createElement('section')
+        section.setAttribute('class','smf')
+        c++
+        menuflutuante.appendChild(section)
+
+    } 
+
+
+}
+Criarsection(3)
+
+
+function mudarImagem(imagem='',i=0){
+    const imgs = [...imagens]
+    imgs[i].src = imagem
+}    
+let imagemdesejada =''
+mudarImagem(imagemdesejada,0)
 
