86f5aee7c98d9d688fcf2fdf2b2e67a9729d7a54
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-16 18:10:39 -0300

    .

---
+++ Meus-Cursos/Programação/Curso Java Script/Desafios/D-01/js/main.js
@@ -0,0 +1,14 @@
+//     Calculadora
+
+//        números
+const numeros = [...document.getElementsByClassName("num")]
+
+
+//       Operações
+const operacoes = document.getElementsByClassName('op')
+
+
+
+//       resultado
+const resultado = document.getElementById("s-3")
+
