a26d14847e8414d86d36d47e06fa0e8b762b4f90
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-16 20:24:23 -0300

    Altulização Curso Java-Script

--- Meus-Cursos/Programação/Curso Java Script/Desafios/D-01/js/main.js
+++ Meus-Cursos/Programação/Curso Java Script/Desafios/D-01/js/main.js
@@ -1,14 +1,32 @@
-//     Calculadora
+//////////////////////////   Calculos \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+const operadores = [(n1,n2)=>{return n1+n2},(n1,n2)=>{return n1-n2},(n1,n2)=>{return n1/n2},(n1,n2)=>{return n1*n2}];
 
-//        números
+//////////////////////////////  números \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+let n1 = 0;
+let n2 = 0;
+
 const numeros = [...document.getElementsByClassName("num")]
-
-
-//       Operações
-const operacoes = document.getElementsByClassName('op')
-
-
-
-//       resultado
 const resultado = document.getElementById("s-3")
+const res = resultado.firstElementChild
 
+//////////////////////////// Operações  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+const operacoes = [...document.getElementsByClassName('op')];
+
+operacoes.map((el,i,a)=>{
+    el.addEventListener('click',()=>{
+        numeros.map((el,i,a)=>{
+            if(i == 1){
+                n1 = Number(el.firstElementChild.value)
+            }else if(i == 2){
+                n2 = Number(el.firstElementChild.value)
+               
+            }
+        })
+        if(n1 == 0 || n2 == 0){
+            alert('Digite um valor para N1 e para N2')
+        }else{
+            res.innerHTML = "Resultado: "+operadores[i](n1,n2)
+        }
+
+    })
+})
