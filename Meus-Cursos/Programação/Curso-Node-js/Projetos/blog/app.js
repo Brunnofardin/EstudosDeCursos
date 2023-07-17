63f1d78ec413b4e41d4070412d384ddf5bba8ef0
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-17 20:43:44 -0300

    .

--- Meus-Cursos/Programação/Curso-Node-js/Projetos/blog/app.js
+++ Meus-Cursos/Programação/Curso-Node-js/Projetos/blog/app.js
@@ -1,33 +1,22 @@
 // Express
-const express = require('express');
-const app = express();
-// Configurações modulos 
+   const express = require("express");
+   const app = express();
 
-   // bodyparser
-      const body_parser = require('body-parser');
-      app.use(body_parser.urlencoded({extended:true}));
-      app.use(body_parser.json());
-
-   // Handlebars
-       const handlebars = require('express-handlebars');
-
-       app.engine('handlebars',handlebars.engine({defaultLayout:'main'}));
-       app.set("view engine",'handlebars');
-       app.set('views','./views')
-      
-         
-   // Mongose
-      const mongoose = require('mongoose');
+// body_parser
+   const BodyParser = require("body-parser");
+   app.use(BodyParser.urlencoded({ extended: false }));
+   app.use(BodyParser.json());
+// Handlebars
+   const handlebars = require('express-handlebars');
+   app.engine('handlebars', handlebars.engine({defaultLayout:'main'}));
+   app.set("view engine",'handlebars');
+   app.set("views",'./views');
 
 // Rotas
-
-app.get('/home',(req,res)=>{
-   res.render('home')
-})
-
-
-// Outros
-
-app.listen(9090,()=>{
-    console.log("Servidor iniciado com sucesso!")
+   app.get('/home',(req,res)=>{
+    res.render('home');
+   })
+// Outros 
+   app.listen(9090,()=>{
+    console.log('Servidor iniciado com sucesso !')
 })
