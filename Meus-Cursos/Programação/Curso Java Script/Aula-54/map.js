1710d6973364c2961d86d29fa828e86e1ac33866
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-16 21:20:07 -0300

    Altulização Curso Java-Script

---
+++ Meus-Cursos/Programação/Curso Java Script/Aula-54/map.js
@@ -0,0 +1,8 @@
+const usuarios = new Map()
+
+usuarios.set("Nome","Brunno Fardin de Souza")
+usuarios.set("Idade",16)
+usuarios.set("Email","Brunnoafrdin@yahoo.com")
+
+console.log(usuarios.delete("Nome"))
+console.log(usuarios)
