ae5bd464d76c0dd7b07e96c30e9233128ddf826d
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-17 14:06:05 -0300

    Altulização Cursno Node Js

--- Meus-Cursos/Programação/Curso-Node-js/Projetos/blog/app.js
+++ Meus-Cursos/Programação/Curso-Node-js/Projetos/blog/app.js
@@ -5,24 +5,31 @@ const handlebars = require('express-handlebars');
 const body_parser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+
 // Configurações
+
    // bodyparser
+
       app.use(body_parser.urlencoded({extended:true}));
       app.use(body_parser.json());
+
    // Handlebars
+
       app.engine('handlebars', handlebars.engine({defaultLayout:'main'}));
       app.set('view engine', 'handlebars');
       app.set('views', './views');
+      
    // Mongose
+
       // em breve
 
 
 // Rotas
-app.get('/home',(req,res)=>{
-    res.render('home')
+   // em breve
 
-})
 
 // Outros
-const port = 8081
-app.listen(port,()=>{console.log("Servidor iniciado com sucesso!")})
+const porta = 9090
+app.listen(porta,()=>{
+    console.log("Servidor iniciado com sucesso!")
+})
