6ce79fba6f2955a64cd2cfa0d83dbfdcb5f5f61a
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-19 14:59:44 -0300

    Altulização Curso Node Js

---
+++ Meus-Cursos/Programação/p010/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+// Configurações
+   // BodyParser
+     const bodyParser = require('body-parser');
+      // Config
+         app.use(bodyParser.json());
+         app.use(bodyParser.urlencoded({extended:false}));
+   //  template handlebars
+     const handlebars = require('express-handlebars');
+      // config
+          app.engine('handlebars',handlebars.engine({defaultLayout:'main'}))
+          app.set('view engine','handlebars')
+          app.set('views','./views')
+    // css
+       //config
+        app.use(express.static(__dirname+'/css'))
+        
+// Rotas
+
+app.get("/",(req,res)=>{
+    res.render('home')
+
+})
+
+
+// Servidor 
+ 
+app.listen(9090,()=>{console.log('Servidor on')})
