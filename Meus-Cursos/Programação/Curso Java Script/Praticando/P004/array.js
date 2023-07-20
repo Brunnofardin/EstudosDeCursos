e5520edd1e2eca63638ace9b342853401ea601c4
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-20 20:18:12 -0300

    .

---
+++ Meus-Cursos/Programação/Curso Java Script/Praticando/P004/array.js
@@ -0,0 +1,11 @@
+
+let cursos = [...document.getElementsByTagName("p")]
+let cursos1 = ['Html5','CSS3','Python','Java-Script']
+cursos.map((el,i,a)=>{
+   for(c = 0;c<=cursos.length;c++){
+
+
+   }
+    
+
+})
