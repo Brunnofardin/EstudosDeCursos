d5aed8204d166d4c556e7f1c9113499a021740b9
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-07-22 09:46:43 -0300

    Altulização Curso Java-Script

--- Meus-Cursos/Programação/Curso Java Script/Desafios/D-02/js/main.js
+++ Meus-Cursos/Programação/Curso Java Script/Desafios/D-02/js/main.js
@@ -1,4 +1,10 @@
+// Variaveis importantes
+
 const recomendacao = document.getElementsByClassName('recomendações')[0]
+const body = document.getElementsByTagName("body")[0]
+
+// Criando itens dinâmicamente
+
 function recomendacoes(){
     for(c=1;c<=2;c++){
         const paragrafo = document.createElement('p')
@@ -15,46 +21,56 @@ function recomendacoes(){
         
     }
 }
-recomendacoes()
-// Descrição nome,idade,etc..
-const botoes = [...document.getElementsByClassName('acao')]
-const sexos = [...document.getElementsByClassName('dados_radios')]
-botoes.map((el,i,a)=>{
-    el.addEventListener('click',()=>{
-        if(i==0){
-            if(nome.value == '' || nome.length == 0 || idade.value=='' || idade.length == 0){
-                alert('Preencha os campos e tente novamente!')
 
-            }else{
-                const p1 = document.getElementById('p1')
-                const p2 = document.getElementById('p2')
-                const nome = document.getElementById('nome')
-                const idade = document.getElementById('idade')
-                console.log('Enviado com sucesso!')
-            }
-        }else if(i==2){
-            if(nome.value == '' || nome.length == 0 || idade.value=='' || idade.length == 0){
-                alert('Preencha os campos e tente novamente!')
+// Chamando função
 
-            }else{
-                sexos.map((el,i,a)=>{
-                    if(i == 0){
-                        if(el.checked){
-                            p1.innerText=`Bem-vindo ${nome.value} pelo que eu entedi o Sr tem ${idade.value} anos e seu sexo é masculino. É uma prezer-Conhecel-lo`
-                            p2.innerText=`Segue abaixo dicas de alimentação`
-                        
-                        }else{
-                            p1.innerText=`Bem-vindo ${nome.value} pelo que eu entedi o Sra tem ${idade.value} anos e seu sexo é femenino. É uma prezer-Conhecel-lá`
-                            p2.innerText=`Segue abaixo dicas de alimentação`
-                        
-                        }       
-                
-                    }     
+body.onload = ()=>{
+    recomendacoes()
+
+    // Coeltando variaveis necessárias
+    const p1 = document.getElementById('p1')
+    const p2 = document.getElementById('p2')
+    const nome = document.getElementById('nome')
+    const idade = document.getElementById('idade')
+    const botoes = [...document.getElementsByClassName('acao')]
+    const sexos = [...document.getElementsByClassName('dados_radios')]
+    
+    //         Apresentando idade nome etc...
+    
+    botoes.map((el,i,a)=>{
+        el.addEventListener('click',()=>{
+            if(i==1){
+                if(nome.value == '' || nome.length == 0 || idade.value=='' || idade.length == 0){
+                    alert('Preencha os campos e tente novamente!')
                     
+                }else if(nome.length <= 2){
+                    alert('Digite seu nome com ao menos 3 caracteres')
+                }else if(Number(idade.value) <= 0){
+                    alert('Não é possivel validar idades negativas ou iguais a zero')
+                }else{
+                    recomendacao.style.display='block'
+                    sexos.map((el,i,a)=>{
+                        if(i == 0){
+                            if(el.checked){
+                                p1.innerText=`Bem-vindo ${nome.value} pelo que eu entedi o Sr tem ${idade.value} anos e seu sexo é masculino. É uma prezer-Conhecel-lo`
+                                p2.innerText=`Segue abaixo dicas de alimentação`
+                            
+                            }else{
+                                p1.innerText=`Bem-vindo ${nome.value} pelo que eu entedi o Sra tem ${idade.value} anos e seu sexo é femenino. É uma prezer-Conhecel-lá`
+                                p2.innerText=`Segue abaixo dicas de alimentação`
+                            
+                            }       
                     
-                })       
-
-            }
-        }    
+                        }          
+                    })       
+    
+                }
+            }else{
+                recomendacao.style.display='none'
+                p1.innerHTML=''
+                p2.innerHTML=''
+            }   
+        })    
     })    
-})    
+    
+}
