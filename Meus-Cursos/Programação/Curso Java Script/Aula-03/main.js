23f32ea3fec8d240fcedf063fab7ccb1546ebe8e
Brunnofardin <128924598+Brunnofardin@users.noreply.github.com>
2023-08-10 10:58:29 -0300

    Altulização curso de nada

--- Meus-Cursos/Programação/Curso Java Script/Aula-03/main.js
+++ Meus-Cursos/Programação/Curso Java Script/Aula-03/main.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 
 /*
 
@@ -8,15 +8,33 @@ let - pode ser usada somente do mesmo escopo para cima
 
 */
 
-function oi(){
-    const nome = 'Brunno'
-    if(true){
-        console.log('Dentro do IF do oi: '+nome)
-        if(true){
-            console.log('Dentro do IF que está no outro IF: '+nome)
-        }
-    }
-    console.log('Dentro do oi e fora do IF: '+nome)
-}
+// function oi(){
+//     const nome = 'Brunno'
+//     if(true){
+//         console.log('Dentro do IF do oi: '+nome)
+//         if(true){
+//             console.log('Dentro do IF que está no outro IF: '+nome)
+//         }
+//     }
+//     console.log('Dentro do oi e fora do IF: '+nome)
+// }
 
-oi()
+// oi()
+
+
+
+
+
+
+
+
+
+
+
+
+
+var numero1 = 2
+var numero2 = 2
+var multiplicacao =  numero1%numero2
+
+console.log("Olá pelo que eu entendi,você tem "+multiplicacao,"como resto da divisão");
